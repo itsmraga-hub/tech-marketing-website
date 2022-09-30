605862f0445dbc6fa83b976603004cd7e52e4197
Raga <ragawilliam570@gmail.com>
2022-09-30 17:40:14 +0300

    Fix eslint error

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
