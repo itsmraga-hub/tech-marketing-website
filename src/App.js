561209991efd57cf92f41e2f3aa648a2d850d4f1
Raga <ragawilliam570@gmail.com>
2022-10-01 19:11:04 +0300

    Add routes to pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Home
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" />
+        <Route path="/signup" />
+        <Route path="contactus" />
+      </Routes>
+    </>
   );
 }
 
